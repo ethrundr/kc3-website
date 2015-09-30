4a93c0ca0f8099119c4b4332a2dbebb530f6d9ec
dragonjet <jetriconew@gmail.com>
2015-09-30 19:59:44 +0800

    Database initialization

--- database/migrations/2015_09_30_092939_initialize.php
+++ database/migrations/2015_09_30_092939_initialize.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Initialize extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->integer('server')->unsigned();
+            $table->bigInteger('game_id')->unsigned();
+            $table->string('game_name', 100);
+            $table->string('username', 100);
             $table->string('email')->unique();
             $table->string('password', 60);
-            $table->rememberToken();
+            $table->text('avatar');
             $table->timestamps();
+            $table->rememberToken();
         });
     }
 
